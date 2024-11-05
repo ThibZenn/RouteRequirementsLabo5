62341d5f8852f5668f1bc1844e5e654747f2454d
ThibZenn <162096780+ThibZenn@users.noreply.github.com>
2024-11-05 19:47:20 +0100

    Added Unit Tests

--- RouteRequirementsUnitTest/UnitTestXRoute.cs
+++ RouteRequirementsUnitTest/UnitTestXRoute.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using RouteRequirementsBL.Exceptions;
@@ -182,7 +183,131 @@ namespace RouteRequirementsUnitTest
             InitializeRoute();
             var (start, route) = _route.ShowRoute();
 
+            Assert.Equal(6, route.Count);
+            Assert.Equal("The Shire", start);
+            Assert.Equal("Edoras", route[2].location);
+            Assert.Equal(35, route[3].distance);
+            Assert.Equal("Mount Doom", route[5].location);
+        }
 
+        [Fact]
+        public void Test_ShowRoute_Between_Given_Locations_Correctly()
+        {
+            InitializeRoute();
+            var (start, route) = _route.ShowRoute("Rivendel", "Minas Tirith");
+
+            Assert.Equal(3, route.Count);
+            Assert.Equal("Rivendel", start);
+            Assert.Equal("Minas Tirith", route[2].location);
+            Assert.Equal(35, route[1].distance);
+        }
+
+        [Fact]
+        public void Test_ShowRoutes_ThrowsException()
+        {
+            InitializeRoute();
+
+            var ex = Assert.Throws<RouteException>(() => _route.ShowRoute("Divendel", "Minas Tirith"));
+            Assert.Equal("Divendel or Minas Tirith doesn't excist in the current route.", ex.Message);
+
+            var ex1 = Assert.Throws<RouteException>(() => _route.ShowRoute("Minas Tirith", "Rivendel"));
+            Assert.Equal("End location must come after start location.", ex1.Message);
+
+            var ex2 = Assert.Throws<RouteException>(() => _route.ShowRoute("Helm's Deep", "Minas Tirith"));
+            Assert.Equal("Helm's Deep or Minas Tirith isn't a stop in the current route.", ex2.Message);
+        }
+
+        [Fact]
+        public void Test_ShowStops_Correctly()
+        {
+            InitializeRoute();
+
+            List<string> stops = _route.ShowStops();
+
+            Assert.Equal(7, stops.Count);
+            Assert.Equal("The Shire", stops[0]);
+            Assert.Equal("Isengard", stops[4]);
+            Assert.Equal("Minas Tirith", stops[5]);
+        }
+
+        [Fact]
+        public void Test_UpdateLocation_Correctly()
+        {
+            // Test als het de eerste is
+            InitializeRoute();
+            _route.UpdateLocation("The Shire", "Shire", true);
+
+            Assert.Equal("Shire", _route._segmentList[0].StopA.Name);
+            Assert.True(_route._segmentList[0].StopA.IsStop);
+
+            //test bij normaal gebruik
+            InitializeRoute(); //reset de route
+            _route.UpdateLocation("Rivendel", "Vendel", false);
+
+            Assert.Equal("Vendel", _route._segmentList[1].StopB.Name);
+            Assert.False(_route._segmentList[1].StopB.IsStop);
+            Assert.Equal("Vendel", _route._segmentList[2].StopA.Name);
+
+        }
+
+        //TODO: UpdateLocation exception tests schrijven
+
+        [Fact]
+        public void Test_InsertLocation_Correctly()
+        {
+            InitializeRoute();
+            _route.InsertLocation("Mordor",15.0,"Isengard",true);
+
+            Assert.Equal(15.0, _route._segmentList[5].Distance);
+            Assert.Equal(13.0, _route._segmentList[6].Distance);
+            Assert.Equal("Mordor", _route._segmentList[5].StopB.Name); // Ensure one location was added
+            Assert.Equal("Mordor", _route._segmentList[6].StopA.Name);
+        }
+
+        [Fact]
+        public void Test_InsertLocation_ThrowsException()
+        {
+            InitializeRoute();
+
+            var ex = Assert.Throws<RouteException>(() => _route.InsertLocation("Mordor", 15.0, "Sisengard", true));
+            Assert.Equal("The fromLocation doesn't exist.", ex.Message);
+
+            var ex1 = Assert.Throws<RouteException>(() => _route.InsertLocation("Mordor", 150.0, "Isengard", true));
+            Assert.Equal("Distance can not be smaller/equal to 0", ex1.Message);
+        }
+
+        [Fact]
+        public void Test_RemoveLocation_Correctly()
+        {
+            //remove first location
+            InitializeRoute();
+            _route.RemoveLocation("The Shire");
+
+            Assert.Equal(7, _route._segmentList.Count);
+
+            //remove last location
+            InitializeRoute();
+            _route.RemoveLocation("Mount Doom");
+
+            Assert.Equal(7, _route._segmentList.Count);
+
+            //remove somewhere in the middle
+            InitializeRoute();
+            _route.RemoveLocation("Isengard");
+
+            Assert.Equal(7, _route._segmentList.Count);
+            Assert.Equal(58.0, _route._segmentList[4].Distance);
+            Assert.Equal("Helm's Deep", _route._segmentList[4].StopA.Name);
+            Assert.Equal("Minas Tirith", _route._segmentList[4].StopB.Name);
+        }
+
+        [Fact]
+        public void Test_RemoveLocation_ThrowsException()
+        {
+            InitializeRoute();
+
+            var ex = Assert.Throws<RouteException>(() => _route.RemoveLocation("SIsengard"));
+            Assert.Equal("The fromLocation doesn't exist.", ex.Message);
         }
     }
 }
